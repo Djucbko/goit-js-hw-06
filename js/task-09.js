258da40ab3818a7520f4b91abb4fed405cd4ce1f
Djucbko <109214332+Djucbko@users.noreply.github.com>
2022-11-28 18:19:21 +0200

    hw-06

---
+++ js/task-09.js
@@ -0,0 +1,14 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const refs = {
+  button: document.querySelector(".change-color"),
+  span: document.querySelector(".color"),
+}
+
+refs.button.addEventListener('click', onChangeColor);
+
+function onChangeColor(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  refs.span.textContent = document.body.style.backgroundColor;
+}
